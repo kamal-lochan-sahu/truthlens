a5be915c83a5587da3139e00c45919c2e6c01cb0
kamal lochan sahu <kamallochansahu.dev@gmail.com>
2026-04-21 01:16:17 +0530

    Removed Vercel logo and added Emoji logo

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-};
+import withPWAInit from 'next-pwa';
 
-export default nextConfig;
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development', // Yeh local PC par PWA off rakhega, Vercel par automatically on kar dega
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default withPWA(nextConfig);
