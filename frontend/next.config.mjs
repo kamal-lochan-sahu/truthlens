bd631a12503f298602b9b2652da568f76152d357
kamal lochan sahu <kamallochansahu.dev@gmail.com>
2026-04-21 01:33:04 +0530

    Removed next-pwa config to fix Vercel build

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,4 @@
-import withPWAInit from 'next-pwa';
-
-const withPWA = withPWAInit({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development', // Yeh local PC par PWA off rakhega, Vercel par automatically on kar dega
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withPWA(nextConfig);
+export default nextConfig;
